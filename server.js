7f109665dffd9aec7b61c32fc0a4ca6e649a3941
ViktorVan22 <zcg5368@163.com>
2022-07-20 18:20:51 +0800

    搭建路由和控制器

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const configure = require("./server/configure");
 app = express();
 // 注册一系列中间件
 app = configure(app);
-app.set("port", process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), () => {
   console.log(`Server is running on http://localhost:${app.get("port")}`);
---
+++ server/routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+// 注册路由
+const router = express.Router();
+// 获取控制器
+const home = require("../controllers/home");
+const image = require("../controllers/image");
+
+module.exports = function (app) {
+  // 定义路由
+  router.get("/", home.index);
+  router.get("/images/:image_id", image.index);
+  router.post("/images", image.create);
+  router.post("/images/:image_id/like", image.like);
+  router.post("/images/:image_id/comment", image.comment);
+
+  // 使用路由中间件
+  app.use(router);
+};
